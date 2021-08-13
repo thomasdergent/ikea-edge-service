798c08711c3c0414e6428ede4cbe959b242c26c6
Thomas Dergent <73995291+thomasdergent@users.noreply.github.com>
2021-08-13 13:29:40 +0200

    Project set-up

---
+++ src/main/java/com/example/ikeaedgeservice/IkeaEdgeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ikeaedgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IkeaEdgeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IkeaEdgeServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ikeaedgeservice/IkeaEdgeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ikeaedgeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IkeaEdgeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
