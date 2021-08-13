7683d649b17271f9f9b5687c85f92f5452d6995e
Thomas Dergent <73995291+thomasdergent@users.noreply.github.com>
2021-08-14 00:37:51 +0200

    Cleanup

--- src/main/java/com/example/ikeaedgeservice/controller/FilledStoreProductController.java
+++ src/main/java/com/example/ikeaedgeservice/controller/FilledStoreProductController.java
@@ -27,6 +27,19 @@ public class FilledStoreProductController {
     @Value("${productservice.baseurl}")
     private String productServiceBaseUrl;
 
+    @GetMapping("/store/{storeName}/article/{articleNumber}")
+    public FilledStoreProduct getRankingByUserIdAndISBN(@PathVariable String storeName, @PathVariable String articleNumber) {
+
+        Store store =
+                restTemplate.getForObject("http://" + storeServiceBaseUrl + "/store/{storeName}",
+                        Store.class, storeName);
+
+        Product product =
+                restTemplate.getForObject("http://" + productServiceBaseUrl + "/store/" + storeName + "/article/" + articleNumber,
+                        Product.class);
+
+        return new FilledStoreProduct(store, product);
+    }
 
     @GetMapping("/meubels/category/{category}")
     public List<FilledStoreProduct> getMeubelsByCategory(@PathVariable String category) {
@@ -62,22 +75,22 @@ public class FilledStoreProductController {
         return new FilledStoreProduct(store, responseEntityProducts.getBody());
     }
 
+//    @PostMapping("/product")
+//    public FilledStoreProduct addProduct(@RequestParam String storeName, @RequestParam String name, @RequestParam String category, @RequestParam String articleNumber, @RequestParam boolean delivery, @RequestParam String material, @RequestParam String maintenance, @RequestParam String environment, @RequestParam int stock, @RequestParam double price, @RequestParam String size) {
+//
+//        Product product = restTemplate.postForObject("http://" + productServiceBaseUrl + "/product", new Product(storeName, name, category, articleNumber, delivery, material, maintenance, environment, stock, price, size), Product.class);
+//
+//        Store store =
+//                restTemplate.getForObject("http://" + storeServiceBaseUrl + "/store/{storeName}",
+//                        Store.class, storeName);
+//
+//
+//        return new FilledStoreProduct(store, product);
+//    }
+
+
     @PostMapping("/product")
-    public FilledStoreProduct addProduct(@RequestParam String storeName, @RequestParam String name, @RequestParam String category, @RequestParam String articleNumber, @RequestParam boolean delivery, @RequestParam String material, @RequestParam String maintenance, @RequestParam String environment, @RequestParam int stock, @RequestParam double price, @RequestParam String size) {
-
-        Product product = restTemplate.postForObject("http://" + productServiceBaseUrl + "/product", new Product(storeName, name, category, articleNumber, delivery, material, maintenance, environment, stock, price, size), Product.class);
-
-        Store store =
-                restTemplate.getForObject("http://" + storeServiceBaseUrl + "/store/{storeName}",
-                        Store.class, storeName);
-
-
-        return new FilledStoreProduct(store, product);
-    }
-
-
-    @PostMapping("/post")
-    public Product addUser(@RequestBody Product product) {
+    public Product addProduct(@RequestBody Product product) {
 
         return restTemplate.postForObject("http://" + productServiceBaseUrl + "/product",
                 product, Product.class);
@@ -85,59 +98,59 @@ public class FilledStoreProductController {
 
     //checken voor updaten WERKT OOK!!!!!!
     @PutMapping("/store/{storeName}/article/{articleNumber}")
-    public Product edit(@PathVariable String storeName, @PathVariable String articleNumber, @RequestBody Product updatedProduct) {
+    public Product updateProduct(@PathVariable String storeName, @PathVariable String articleNumber, @RequestBody Product updatedProduct) {
 
         ResponseEntity<Product> responseEntityReview =
-                restTemplate.exchange("http://" + productServiceBaseUrl + "/edit/" + storeName + "/article/" + articleNumber,
+                restTemplate.exchange("http://" + productServiceBaseUrl + "/store/" + storeName + "/article/" + articleNumber,
                         HttpMethod.PUT, new HttpEntity<>(updatedProduct), Product.class);
 
         return responseEntityReview.getBody();
     }
 
-    //werkt basic
-    @PutMapping("/products")
-    public Product update(@RequestBody Product updatedProduct) {
+//    //werkt basic
+//    @PutMapping("/products")
+//    public Product update(@RequestBody Product updatedProduct) {
+//
+//        ResponseEntity<Product> responseEntityReview =
+//                restTemplate.exchange("http://" + productServiceBaseUrl + "/product",
+//                        HttpMethod.PUT, new HttpEntity<>(updatedProduct), Product.class);
+//
+//        return responseEntityReview.getBody();
+//    }
 
-        ResponseEntity<Product> responseEntityReview =
-                restTemplate.exchange("http://" + productServiceBaseUrl + "/product",
-                        HttpMethod.PUT, new HttpEntity<>(updatedProduct), Product.class);
 
-        return responseEntityReview.getBody();
-    }
+//    //werkt maar is tering lang
+//    @PutMapping("/product")
+//    public FilledStoreProduct updateProduct(@RequestParam String storeName, @RequestParam String name, @RequestParam String category, @RequestParam String articleNumber, @RequestParam boolean delivery, @RequestParam String material, @RequestParam String maintenance, @RequestParam String environment, @RequestParam int stock, @RequestParam double price, @RequestParam String size) {
+//
+//        Product product =
+//                restTemplate.getForObject("http://" + productServiceBaseUrl + "/product/store/" + storeName + "/article/" + articleNumber,
+//                        Product.class);
+//        product.setName(name);
+//        product.setCategory((category));
+//        product.setArticleNumber(articleNumber);
+//        product.setDelivery(delivery);
+//        product.setMaterial(material);
+//        product.setMaintenance(maintenance);
+//        product.setEnvironment(environment);
+//        product.setStock(stock);
+//        product.setPrice(price);
+//        product.setSize(size);
+//
+//        ResponseEntity<Product> responseEntityReview =
+//                restTemplate.exchange("http://" + productServiceBaseUrl + "/product",
+//                        HttpMethod.PUT, new HttpEntity<>(product), Product.class);
+//
+//        Product retrievedProduct = responseEntityReview.getBody();
+//
+//        Store store =
+//                restTemplate.getForObject("http://" + storeServiceBaseUrl + "/store/{storeName}",
+//                        Store.class, storeName);
+//
+//        return new FilledStoreProduct(store, retrievedProduct);
+//    }
 
-
-    //werkt maar is tering lang
-    @PutMapping("/product")
-    public FilledStoreProduct updateProduct(@RequestParam String storeName, @RequestParam String name, @RequestParam String category, @RequestParam String articleNumber, @RequestParam boolean delivery, @RequestParam String material, @RequestParam String maintenance, @RequestParam String environment, @RequestParam int stock, @RequestParam double price, @RequestParam String size) {
-
-        Product product =
-                restTemplate.getForObject("http://" + productServiceBaseUrl + "/product/store/" + storeName + "/article/" + articleNumber,
-                        Product.class);
-        product.setName(name);
-        product.setCategory((category));
-        product.setArticleNumber(articleNumber);
-        product.setDelivery(delivery);
-        product.setMaterial(material);
-        product.setMaintenance(maintenance);
-        product.setEnvironment(environment);
-        product.setStock(stock);
-        product.setPrice(price);
-        product.setSize(size);
-
-        ResponseEntity<Product> responseEntityReview =
-                restTemplate.exchange("http://" + productServiceBaseUrl + "/product",
-                        HttpMethod.PUT, new HttpEntity<>(product), Product.class);
-
-        Product retrievedProduct = responseEntityReview.getBody();
-
-        Store store =
-                restTemplate.getForObject("http://" + storeServiceBaseUrl + "/store/{storeName}",
-                        Store.class, storeName);
-
-        return new FilledStoreProduct(store, retrievedProduct);
-    }
-
-    @DeleteMapping("/product/store/{storeName}/article/{articleNumber}")
+    @DeleteMapping("/store/{storeName}/article/{articleNumber}")
     public ResponseEntity deleteProduct(@PathVariable String storeName, @PathVariable String articleNumber) {
 
         restTemplate.delete("http://" + productServiceBaseUrl + "/product/store/" + storeName + "/article/" + articleNumber);
