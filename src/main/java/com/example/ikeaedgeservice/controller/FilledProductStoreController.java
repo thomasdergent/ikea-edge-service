552a403e023f24bf4f8167b31e172939c3ca7aa6
Thomas Dergent <73995291+thomasdergent@users.noreply.github.com>
2021-08-26 20:17:25 +0200

    CORS

--- src/main/java/com/example/ikeaedgeservice/controller/FilledProductStoreController.java
+++ src/main/java/com/example/ikeaedgeservice/controller/FilledProductStoreController.java
@@ -16,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class FilledProductStoreController {
 
